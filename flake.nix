0b8d1d447864f7c1ff92875e7697f8d3d04f9a1c
Matt Camp <matt@aicampground.com>
2024-02-21 13:49:22 -0600

    try this

--- flake.nix
+++ flake.nix
@@ -1,17 +1,33 @@
 {
   outputs = { self, nixpkgs, ... }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux"; # Default system
+      darwinSystem = "x86_64-darwin";
+      linuxSystem = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      darwinPkgs = import nixpkgs {
+        system = darwinSystem;
+      };
+
+      linuxPkgs = import nixpkgs {
+        system = linuxSystem;
+      };
     in
     {
       nixosModules.default = import ./module.nix;
-      packages.x86_64-linux.scientific-fhs = pkgs.callPackage ./fhs.nix {
+
+      packages.x86_64-linux.scientific-fhs = linuxPkgs.callPackage ./fhs.nix {
         enableNVIDIA = false;
         enableGraphical = true;
         juliaVersion = "1.10.0";
       };
-      packages.x86_64-darwin.scientific-fhs = pkgs.callPackage ./fhs.nix {
-        enableNVIDIA = false;
+
+      packages.x86_64-darwin.scientific-fhs = darwinPkgs.callPackage ./fhs.nix {
+        enableNVIDIA = false; # Assuming NVIDIA support is irrelevant for Darwin, adjust if needed
         enableGraphical = true;
         juliaVersion = "1.10.0";
       };
